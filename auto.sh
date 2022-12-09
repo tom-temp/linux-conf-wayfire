c0761d23fc8ea9a4886dba8ef90c01bced3d5c86
test-ning <aaa@aaa.com>
2022-12-09 16:05:18 +0800

    1

---
+++ auto.sh
@@ -0,0 +1,13 @@
+
+
+export WLR_NO_HARDWARE_CURSORS=1
+export LIBGL_ALWAYS_SOFTWARE=1
+export QT_QPA_PLATFORM=wayland-egl
+export ELM_DISPLAY=wl
+export SDL_VIDEODRIVER=wayland
+
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=wayfire
+export XDG_CURRENT_DESKTOP=wayfire
+export MOZ_ENABLE_WAYLAND=1
+export XCURSOR_PATH=/usr/share/icons:~/.local/share/icons
